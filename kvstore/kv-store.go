cdc1fb9cf5f6074eb8a8b2aeee51ea5981409a5f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-30 20:16:02 -0400

    Update kv-store.go

--- kvstore/kv-store.go
+++ kvstore/kv-store.go
@@ -1,1 +1,40 @@
 
+package kvstore
+
+import (
+	"github.com/radhika-singh-10/Raft-Key-Value-Store/logger"
+)
+
+type KeyValueStore struct {
+	PersistentStore PersistentStore
+}
+
+func NewKeyValueStore(store PersistentStore) *KeyValueStore {
+	return &KeyValueStore{store}
+}
+
+func (kv *KeyValueStore) Set(key, value string) {
+	err := kv.PersistentStore.Set(key, value)
+	if err != nil {
+		logger.Log.Errorln("Unable to add key value please check ")
+	}
+}
+
+func (kv *KeyValueStore) Get(key string) (string, bool) {
+	return kv.PersistentStore.Get(key)
+}
+
+func (kv *KeyValueStore) Delete(key string) {
+	err := kv.PersistentStore.Delete(key)
+	if err != nil {
+		logger.Log.Errorln("Unable to delete key please check ")
+	}
+}
+
+func (kv *KeyValueStore) Dump() map[string]string {
+	return kv.PersistentStore.Dump()
+}
+
+func (kv *KeyValueStore) Restore(data map[string]string) error {
+	return kv.PersistentStore.Restore(data)
+}

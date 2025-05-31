420cb37ceb0459280683fee466e17d3d00e42821
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-30 20:16:37 -0400

    Update log.go

--- logger/log.go
+++ logger/log.go
@@ -1,1 +1,15 @@
+package logger
 
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var Log *logrus.Logger
+
+func init() {
+	Log = logrus.New()
+	Log.SetFormatter(&logrus.JSONFormatter{})
+	Log.SetLevel(logrus.DebugLevel)
+	Log.SetOutput(os.Stdout)
+}

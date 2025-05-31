ae14f1dbf847962b97220dc3bc1df83d462b4fb0
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-30 20:14:53 -0400

    Update peer-server.go

--- httpapi/peer-server.go
+++ httpapi/peer-server.go
@@ -1,1 +1,23 @@
+package httpapi
 
+import (
+	"github.com/radhika-singh-10/Raft-Key-Value-Store/logger"
+	"github.com/radhika-singh-10/Raft-Key-Value-Store/raftnode"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type PeerServer struct {
+	RaftNode *raftnode.RaftNode
+}
+
+func (ps *PeerServer) ServeHTTP(peerListenURL string) {
+	r := mux.NewRouter()
+	r.HandleFunc("/raft", ps.RaftNode.Transport.Receive).Methods("POST")
+
+	peerAddr := stripHTTPPrefix(peerListenURL)
+	logger.Log.Printf("Starting peer HTTP server on %s", peerAddr)
+	if err := http.ListenAndServe(peerAddr, r); err != nil && err != http.ErrServerClosed {
+		logger.Log.Fatalf("ListenAndServe(): %v", err)
+	}
+}

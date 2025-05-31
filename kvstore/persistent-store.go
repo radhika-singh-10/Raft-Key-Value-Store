d3da1b9bd36edc488f541be9e31b90681cbf06e7
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-30 20:16:17 -0400

    Update persistent-store.go

--- kvstore/persistent-store.go
+++ kvstore/persistent-store.go
@@ -1,1 +1,9 @@
+package kvstore
 
+type PersistentStore interface {
+	Set(key, value string) error
+	Get(key string) (string, bool)
+	Delete(key string) error
+	Dump() map[string]string
+	Restore(data map[string]string) error
+}
